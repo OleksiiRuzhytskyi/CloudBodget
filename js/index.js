3943e7ffea96d912b9230a5bccd04a6d3c64e116
Oleksii Ruzhytskyi <oleksiiruzhytskyi@gmail.com>
2021-02-23 20:11:54 +0200

    Added sample markup

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// $(document).ready(function() {
+//     $('.main-burger,.main-nav').toggleClass('active');
+// });
+// $('.main-burger__ico,.main-nav').on('click', function(e) {
+//     e.preventDefault;
+//     $(this).toggleClass('active')
+// });
